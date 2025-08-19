770b5e624d016e413d91241061eb5e9aad2c03f9
isaac7708266-droid <isaac7708266@gmail.com>
2025-08-19 13:30:39 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const settings = {
   packname: 'COOLICE',
   author: '‎',
   botName: "COOLICE",
-  botOwner: 'COOLICE, // Your name
+  botOwner: 'COOLICE', // Your name
   ownerNumber: '233536714265', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
